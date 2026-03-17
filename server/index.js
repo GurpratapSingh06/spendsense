6942a2e32bdefe8bb8ad18acee11f2757aed4c99
Gurpratap Singh Sandhu <gsandhu1307@gmail.com>
2026-03-17 16:16:48 +0530

    fix CORS for production

--- server/index.js
+++ server/index.js
@@ -14,14 +14,23 @@ const aiRoutes = require('./routes/ai');
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    process.env.CLIENT_URL
-  ],
+  origin: function(origin, callback) {
+    const allowed = [
+      'http://localhost:5173',
+      'https://spendsense-tkne.vercel.app',
+      process.env.CLIENT_URL
+    ].filter(Boolean)
+    
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}))
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 
